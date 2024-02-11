8c24fdb617a09fe9f771848cb096b848474b3191
knightmoon <ankitakshay78@gmail.com>
2024-02-11 17:26:55 +0530

    4.0

--- index.js
+++ index.js
@@ -18,15 +18,50 @@ function calculateIncome(){
     return reduPercIncome;
 }
 
+function faimlyComposition(){
+    var members = document.getElementById('members').value;
+
+    if(members == 1){
+        return 1;
+    }if(members == 2){
+        return 1.57;
+    }if(members == 3){
+        return 2.04;
+    }if(members == 4){
+        return 2.46;
+    }if(members == 5){
+        return 2.85;
+    }if((members - 5)>0){
+        var m = members-5
+        return 2.85 + (0.35*m);
+    }
+}
+
+function calDisability(){
+    var dismembers = document.getElementById('dismembers').value;
+    var faimMem= faimlyComposition();
+
+    return (dismembers*0.5)+faimMem;
+}
+
 function submit(){
     var annual = document.getElementById('annual').value;
+
     var asset = calculateAsset();
     var income = calculateIncome();
+    var faimMem = faimlyComposition();
+    var disMem = calDisability();
 
     console.log(asset)
     console.log(income)
+    console.log(faimMem)
+    console.log(disMem)
 
-    var sums = parseFloat(annual) + asset + income;  // make annual behave like float
+    var issecal = document.getElementById('isseValue');
+    var sums = (parseFloat(annual) + asset + income)/faimMem+disMem;  // make annual behave like float
+    issecal.innerHTML = sums;
+
+
     console.log(sums);
 }
 
